f64c1e0593a76c4db0c619f80e448e707573fa7f
haseebansarii <97794655+haseebansarii@users.noreply.github.com>
2025-04-29 03:29:06 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1166,11 +1166,23 @@ export default function Home() {
                         <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
                       </svg>
                     </a>
-                    <a href="https://www.fiverr.com/s/NNlRBxN" className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-100 dark:bg-gray-700 text-gray-600 dark:text-gray-300 hover:bg-primary-500 hover:text-white dark:hover:bg-primary-500 transition-colors">
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
+                   <a 
+  href="https://www.fiverr.com/s/NNlRBxN" 
+  target="_blank" 
+  rel="noopener noreferrer"
+  className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-100 dark:bg-gray-700 text-gray-600 dark:text-gray-300 hover:bg-primary-500 hover:text-white dark:hover:bg-primary-500 transition-colors"
+>
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    width="20" 
+    height="20" 
+    fill="currentColor" 
+    viewBox="0 0 24 24"
+  >
     <path d="M20.678 0H3.322A3.325 3.325 0 000 3.322v17.356A3.325 3.325 0 003.322 24h17.356A3.325 3.325 0 0024 20.678V3.322A3.325 3.325 0 0020.678 0zM12.74 17.856h-1.931v-4.717H9.406v4.717H7.475V6.272h1.931v5.285h1.403V6.272h1.931v11.584zM18.135 12.715h-2.262v2.68h2.262v2.461h-4.193V6.272h4.193v2.46h-2.262v1.524h2.262v2.459z"/>
   </svg>
 </a>
+
 
                   </div>
                 </div>

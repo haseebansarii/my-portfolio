4ea7445cadae83b809cd256b84ca36dae52757fc
Mohammad Haseeb <ansarihaseeb615@gmail.com>
2026-02-16 15:19:52 +0500

    double

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ export default function App() {
         <HeroSection />
         <div className="relative">
           <AboutSection />
-          <StatsSection />
+          {/* <StatsSection /> */}
           <ExperienceSection />
           <SkillsSection />
           <ProjectsSection />
